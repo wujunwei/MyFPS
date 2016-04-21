e85a130b0cc3914109cc6e3ed0c8e365b4f5bab1
Adam123 <wjw3323@live.com>
2016-04-21 11:16:14 +0800

    添加地图文件，及读取文件操作

--- Assets/Scripts/A_alogrithm.cs
+++ Assets/Scripts/A_alogrithm.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
 public class A_alogrithm : MonoBehaviour {
     public Transform enemy;
@@ -10,32 +11,47 @@ public class A_alogrithm : MonoBehaviour {
 	void Start () {
         is_wolking = true;
         enemy = this.transform.parent;
+        InitMap();
     }
 
-	void InitMap()
-	{
+	void InitMap() {
+        //读取地图文件
+        StreamReader sr = new StreamReader("./Assets/StreamingAssets/map.txt", Encoding.Default);
+        for(int j=-42; j<42;j++)
+        {
+            Dictionary<int, int> temp = new Dictionary<int, int>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //Debug.Log(line.ToString());
 
-	}
+            }
+            for(int i=-48; i<48;i++)
+            {
+                
+                
+            }
+            map.Add(j, temp);
+        }
+        
+        
+    }
 
-    public void Move(float speed)
-    {
+    public void Move(float speed) {
         if (enemy != null && is_wolking)
         {
 
         }
     }
-   public void SetDestination(Vector3 play_position)
-    {
+   public void SetDestination(Vector3 play_position) {
         this.destination = play_position;
     }
-    public void ResetPath()
-    {
+    public void ResetPath() {
         is_wolking = false;
     }
 
 	// Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
